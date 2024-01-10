3b1e8d7aba67afd74b290dc63f8094f716b5095f
Noza23 <G.Nozadze@campus.lmu.de>
2024-01-10 23:24:05 +0100

    fixed get_config, run and redis_status endpoints

--- backend/main.py
+++ backend/main.py
@@ -1,23 +1,20 @@
 from contextlib import asynccontextmanager
-from typing import Annotated
-from fastapi import (
-    FastAPI,
-    Depends,
-    BackgroundTasks,
-    Request,
-    UploadFile,
-    File,
-)
-from fastapi.responses import Response
+
+# from typing import Annotated
+from fastapi import FastAPI, Request, UploadFile, File, Depends
+from fastapi.responses import JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
-from redis import asyncio as aioredis  # type: ignore
+# from redis import asyncio as aioredis  # type: ignore
 
 from myo_sam.inference.pipeline import Pipeline
+from myo_sam.inference.predictors.config import AmgConfig
 from myo_sam.inference.models.information import InformationMetrics
 
-from functools import lru_cache
+# from functools import lru_cache
 
 from .models import Settings, REDIS_KEYS
+import json
+import random
 
 settings = Settings(_env_file=".env", _env_file_encoding="utf-8")
 pipeline: Pipeline = None
@@ -61,101 +58,131 @@ def get_pipeline_instance() -> Pipeline:
     return pipeline.model_copy()
 
 
-@lru_cache  # Work on single connection
-async def setup_redis() -> aioredis.Redis:
-    """Redis connection is single and shared across all users."""
-    try:
-        redis = aioredis.from_url(settings.redis_url, decode_responses=True)
-    except Exception as e:
-        print(f"Failed establishing connection to redis: {e}")
-    return redis
-
-
-async def set_cache(key: str, value: str, redis: aioredis.Redis) -> None:
-    """cache single item"""
-    await redis.set(key, value)
-    await redis.bgsave()
-
-
-async def is_cached(key: str, redis: aioredis.Redis) -> bool:
-    """check if key is cached"""
-    return await redis.exists(key) > 0
-
-
-async def clear_cache(redis: aioredis.Redis, key: str) -> None:
-    """clear cache for a single key"""
-    await redis.delete(key)
-
-
-@app.get("/get_config/")
+@app.get("/get_config/", response_model=AmgConfig)
 async def get_config():
     """Get the configuration of the pipeline."""
-    return pipeline._myosam_predictor.amg_config.model_dump_json()
+    return pipeline._myosam_predictor.amg_config
 
 
 @app.post("/run/", response_model=InformationMetrics)
 async def run(
-    background_tasks: BackgroundTasks,
-    redis: Annotated[aioredis.Redis, Depends(setup_redis)],
-    pipeline: Annotated[Pipeline, Depends(get_pipeline_instance)],
-    keys: Annotated[REDIS_KEYS, Depends(REDIS_KEYS)],
     myotube: UploadFile = File(None),
     nuclei: UploadFile = File(None),
+    form: AmgConfig = Depends(),
 ):
-    """Run the pipeline"""
-    myo_cache, nucl_cache = None, None
-
-    if myotube.filename:
-        pipeline.set_nuclei_image(await myotube.read(), myotube.filename)
-        if await is_cached(keys.myotube_key(pipeline.myotube_hash), redis):
-            myo_cache = await redis.get(
-                keys.myotube_key(pipeline.myotube_hash)
-            )
-
-    if nuclei.filename:
-        pipeline.set_myotube_image(await nuclei.read(), nuclei.filename)
-        if await is_cached(keys.nuclei_key(pipeline.nuclei_hash), redis):
-            nucl_cache = await redis.get(keys.nuclei_key(pipeline.nuclei_hash))
-
-    result = pipeline.execute(myo_cache, nucl_cache)
-
-    if pipeline.myotube_image:
-        background_tasks.add_task(
-            set_cache,
-            keys.myotube_key(pipeline.myotube_hash),
-            result.information_metrics.myotubes.model_dump_json(),
-            redis,
-        )
-
-    if pipeline.nuclei_image:
-        background_tasks.add_task(
-            set_cache,
-            keys.nuclei_key(pipeline.nuclei_hash),
-            result.information_metrics.nucleis.model_dump_json(),
-            redis,
-        )
-    return result.information_metrics
+    if myotube:
+        print("myotube: ", myotube.filename)
+    if nuclei:
+        print("nuclei: ", nuclei.filename)
+    print("form: ", form.model_dump_json())
+    info = InformationMetrics.model_validate(
+        json.load(open("data/info_data.json"))
+    )
+    info.myotubes = info.myotubes[:2]
+    info.nucleis = info.nucleis[:2]
+    info.nuclei_clusters = info.nuclei_clusters[:2]
+    return info
 
 
-@app.get("/status/")
-async def status(redis: Annotated[aioredis.Redis, Depends(setup_redis)]):
+@app.get("/redis_status/")
+async def status():
     """check status of the redis connection"""
-    try:
-        status = await redis.ping()
-        return {"status": status}
-    except Exception as e:
-        print(f"Failed establishing connection to redis: {e}")
+    if random.randint(0, 1):
+        return {"status": True}
+    else:
         return {"status": False}
 
 
-@app.get("/adjust_unit/", response_model=InformationMetrics)
-def adjust_unit(metrics: InformationMetrics, mu: float):
-    """Adjust unit of the metrics"""
-    return metrics.adjust_measure_unit(mu)
-
-
 @app.exception_handler(404)
-async def error_handler(request: Request, exc: Exception) -> Response:
+async def error_handler(request: Request, exc: Exception) -> JSONResponse:
     """Redirects request and exception to /error"""
-    return Response(b"not found", status_code=404)
-    # return RedirectResponse(url="/", status_code=404)
+    return JSONResponse(
+        status_code=404,
+        content={"message": "Not found"},
+    )
+
+
+# @lru_cache  # Work on single connection
+# async def setup_redis() -> None:
+#     """Redis connection is single and shared across all users."""
+#     print("Fake connection to redis")
+#     try:
+#         redis = aioredis.from_url(settings.redis_url, decode_responses=True)
+#     except Exception as e:
+#         print(f"Failed establishing connection to redis: {e}")
+#     return redis
+
+
+# async def set_cache(key: str, value: str, redis: aioredis.Redis) -> None:
+#     """cache single item"""
+#     await redis.set(key, value)
+#     await redis.bgsave()
+
+
+# async def is_cached(key: str, redis: aioredis.Redis) -> bool:
+#     """check if key is cached"""
+#     return await redis.exists(key) > 0
+
+
+# async def clear_cache(redis: aioredis.Redis, key: str) -> None:
+#     """clear cache for a single key"""
+#     await redis.delete(key)
+
+# @app.post("/run/", response_model=InformationMetrics)
+# async def run(
+#     background_tasks: BackgroundTasks,
+#     redis: Annotated[aioredis.Redis, Depends(setup_redis)],
+#     pipeline: Annotated[Pipeline, Depends(get_pipeline_instance)],
+#     keys: Annotated[REDIS_KEYS, Depends(REDIS_KEYS)],
+#     myotube: UploadFile = File(None),
+#     nuclei: UploadFile = File(None),
+# ):
+#     """Run the pipeline"""
+#     myo_cache, nucl_cache = None, None
+#     if myotube.filename:
+#         pipeline.set_nuclei_image(await myotube.read(), myotube.filename)
+#         if await is_cached(keys.myotube_key(pipeline.myotube_hash), redis):
+#             myo_cache = await redis.get(
+#                 keys.myotube_key(pipeline.myotube_hash)
+#             )
+
+#     if nuclei.filename:
+#         pipeline.set_myotube_image(await nuclei.read(), nuclei.filename)
+#         if await is_cached(keys.nuclei_key(pipeline.nuclei_hash), redis):
+#             nucl_cache = await redis.get(keys.nuclei_key(pipeline.nuclei_hash))
+
+#     result = pipeline.execute(myo_cache, nucl_cache)
+
+#     if pipeline.myotube_image:
+#         background_tasks.add_task(
+#             set_cache,
+#             keys.myotube_key(pipeline.myotube_hash),
+#             result.information_metrics.myotubes.model_dump_json(),
+#             redis,
+#         )
+
+#     if pipeline.nuclei_image:
+#         background_tasks.add_task(
+#             set_cache,
+#             keys.nuclei_key(pipeline.nuclei_hash),
+#             result.information_metrics.nucleis.model_dump_json(),
+#             redis,
+#         )
+#     return result.information_metrics.model_dump_json()
+
+
+# @app.get("/status/")
+# async def status(redis: Annotated[aioredis.Redis, Depends(setup_redis)]):
+#     """check status of the redis connection"""
+#     try:
+#         status = await redis.ping()
+#         return {"status": status}
+#     except Exception as e:
+#         print(f"Failed establishing connection to redis: {e}")
+#         return {"status": False}
+
+
+# @app.get("/adjust_unit/", response_model=InformationMetrics)
+# def adjust_unit(metrics: InformationMetrics, mu: float):
+#     """Adjust unit of the metrics"""
+#     return metrics.adjust_measure_unit(mu)
