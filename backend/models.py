15ec7119b5a40abf6eec83f180c6e15520a744e0
Noza23 <G.Nozadze@campus.lmu.de>
2024-01-13 11:55:01 +0100

    Validation and Inference responses + image_hash is sufficient

--- backend/models.py
+++ backend/models.py
@@ -1,9 +1,11 @@
 from pydantic_settings import BaseSettings
-from pydantic import BaseModel, model_validator
+from pydantic import BaseModel, model_validator, Field
 
 from enum import Enum
+from typing import Union
 import json
 
+from myo_sam.inference.models.information import InformationMetrics
 from myo_sam.inference.predictors.config import AmgConfig
 
 
@@ -11,7 +13,7 @@ class Config(BaseModel):
     """Configuration for the pipeline."""
 
     amg_config: AmgConfig
-    measure_unit: float
+    measure_unit: float = Field(description="The measure unit in pixels.")
 
     @model_validator(mode="before")
     @classmethod
@@ -34,19 +36,39 @@ class ENDPOINTS(str, Enum):
     INFERENCE = "inference"
 
 
+class ValidationResponse(BaseModel):
+    """Validation response."""
+
+    roi_coords: list[list[int]] = Field(description="Contours of the ROI.")
+    state: dict[str, list[int]] = Field(description="validation state.")
+    hash_str: str = Field(description="The hash string of the image.")
+
+
+class InferenceResponse(BaseModel):
+    """Inference response."""
+
+    information_data: InformationMetrics
+    hash_str_myotube: Union[str, None] = Field(
+        description="The hash string of the myotube image."
+    )
+    hash_str_nuclei: str[str, None] = Field(
+        description="The hash string of the nuclei image."
+    )
+
+
 class REDIS_KEYS:
     """Methods to generate key names for Redis data."""
 
     def __init__(self, prefix: str = "myovision"):
         self.prefix = prefix
 
-    def myotube_key(self, hash_str: str) -> str:
-        """A key for myotube image."""
-        return f"{self.prefix}:myotube:{hash_str}"
+    def image_hash_key(self, hash_str: str) -> str:
+        """A key for image hash."""
+        return f"{self.prefix}:image:{hash_str}"
 
-    def nuclei_key(self, hash_str: str) -> str:
-        """A key for myotube mask."""
-        return f"{self.prefix}:nuclei:{hash_str}"
+    def image_path_key(self, hash_str: str) -> str:
+        """A key for image path."""
+        return f"{self.prefix}:image_path:{hash_str}"
 
     def state_key(self, hash_str: str) -> str:
         """A key for state."""
