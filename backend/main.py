d17e5e9a48deb96d1590c517544aac70a7328a14
Noza23 <G.Nozadze@campus.lmu.de>
2024-01-16 15:11:09 +0100

    starting validation mock endpoint

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,9 @@
 from contextlib import asynccontextmanager
 
-from fastapi import FastAPI, UploadFile, File, HTTPException
+from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-from .models import Settings, REDIS_KEYS, Config, InferenceResponse
-import json
+from .models import Settings, REDIS_KEYS, Config
 import random
 
 settings = Settings(_env_file=".env", _env_file_encoding="utf-8")
@@ -57,63 +56,3 @@ async def status():
         return {"status": True}
     else:
         return {"status": False}
-
-
-@app.post("/inference/", response_model=InferenceResponse)
-async def run_inference(
-    config: Config,
-    myotube: UploadFile = File(None),
-    nuclei: UploadFile = File(None),
-):
-    """Run the pipeline in inference mode."""
-    if not myotube.filename and not nuclei.filename:
-        raise HTTPException(
-            status_code=400,
-            detail="Either myotube or nuclei image must be provided.",
-        )
-
-    # myo_cache, nucl_cache = None, None
-    # if myotube.filename:
-    #     pipeline.set_myotube_image(await myotube.read(), myotube.filename)
-    #     img_hash = pipeline.myotube_hash
-    #     if await is_cached(keys.result_key(img_hash), redis):
-    #         myo_cache = await redis.get(keys.result_key(img_hash))
-    #         path = await redis.get(keys.image_path_key(img_hash))
-    #         if not path:
-    #             # path might be cleaned by regular image cleaning
-    #             path = get_fp(settings.images_dir)
-    #             _ = pipeline.save_myotube_image(path)
-    #             background_tasks.add_task(
-    #                 set_cache, {keys.image_path_key(img_hash): path}, redis
-    #             )
-    # if nuclei.filename:
-    #     pipeline.set_nuclei_image(await nuclei.read(), nuclei.filename)
-    #     sec_img_hash = pipeline.nuclei_hash
-    #     if await is_cached(keys.result_key(sec_img_hash), redis):
-    #         nucl_cache = await redis.get(keys.result_key(sec_img_hash))
-
-    # # Execute Pipeline
-    # pipeline._myosam_predictor.update_amg_config(config.amg_config)
-    # pipeline.set_measure_unit(config.general_config.measure_unit)
-    # result = pipeline.execute(myo_cache, nucl_cache).information_metrics
-    # myos, nucls = result.myotubes, result.nucleis
-
-    # if myotube.filename and not myo_cache:
-    #     background_tasks.add_task(
-    #         set_cache,
-    #         {keys.result_key(sec_img_hash): myos.model_dump_json()},
-    #     )
-
-    # if nuclei.filename and not nucl_cache:
-    #     background_tasks.add_task(
-    #         set_cache,
-    #         {keys.result_key(sec_img_hash): nucls.model_dump_json()},
-    #     )
-
-    # # Overlay contours on main image
-    # img_drawn = pipeline.draw_contours_on_myotube_image(myos, nucls)
-    # path = get_fp(settings.images_dir)
-    # pipeline.save_myotube_image(path, img_drawn)
-    # return InferenceResponse(
-    #     iamge_path=path, image_hash=img_hash, secondary_image_hash=sec_img_hash
-    # )
