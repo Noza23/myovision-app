8e053cf5905098dd0b37e95a9abf833cc2cb4fd7
Noza23 <G.Nozadze@campus.lmu.de>
2024-04-16 14:30:00 +0200

    refactor

--- backend/utils.py
+++ backend/utils.py
@@ -3,17 +3,19 @@ import uuid
 import os
 
 
-def get_fp(base: str, suffix: str = ".png") -> str:
-    return os.path.join(base, f"{uuid.uuid4().hex}{suffix}")
+def get_fp(directory: str, suffix: str = ".png") -> str:
+    """Get UUID based file path."""
+    return os.path.join(directory, f"{uuid.uuid4().hex}{suffix}")
 
 
-def clean_dir(base: str) -> None:
-    for f in os.listdir(base):
-        os.remove(os.path.join(base, f))
+def clean_dir(directory: str) -> None:
+    """Clean the directory."""
+    for f in os.listdir(directory):
+        os.remove(os.path.join(directory, f))
 
 
 def preprocess_ws_resp(data: dict[str, Any], exclude: list[str] = []) -> dict:
-    """preprocess data before sending to front."""
+    """preprocess data before sending through websocket."""
     data_post: dict[str, Any] = {}
     for k, v in data.items():
         if k not in exclude:
