180e9a7b4661aad758b6205deafa874556c56e10
Noza23 <G.Nozadze@campus.lmu.de>
2024-04-16 14:20:41 +0200

    refactor

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,10 +1,6 @@
 # This source code is licensed under the license found in the
 # LICENSE file in the root directory of this source tree.
-from __future__ import annotations
-
-
 from .models import Settings, REDIS_KEYS
-
 
 SETTINGS = Settings(_env_file=".env", _env_file_encoding="utf-8")
 KEYS = REDIS_KEYS(prefix="myovision")
